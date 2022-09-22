bf8855cea28eb873bb76aaa9e55a0dfb23cb955b
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-09-21 22:50:40 -0400

    Call cached DFT in FFT for low N

--- src/pset1/problem2.jl
+++ src/pset1/problem2.jl
@@ -33,7 +33,7 @@ end
 plt_m1 = plot(Ns, DFT_time, label = "DFT", xlabel = "N", ylabel = "time [sec]",
               axis = :log10, minorgrid = true, markershape = :circle, title = "M = $M",
               legend = :topleft)
-plot!(plt_m1, Ns, FFT_time, label = "FFT", markershape = :circle)
+plot!(plt_m1, Ns, FFT_time, label = "FFT", markershape = :rect)
 
 DFT_cache = map(DFT_matrix, Ns)
 
@@ -50,7 +50,7 @@ for (i, N) in enumerate(Ns)
     local x = rand(ComplexF64, N, M)
     cached_DFT_time[i] = @belapsed apply_cached_DFT($x)
 end
-plot!(plt_m1, Ns, cached_DFT_time, label = "DFT cached", markershape = :circle)
+plot!(plt_m1, Ns, cached_DFT_time, label = "cached DFT", markershape = :star5)
 
 M = 100
 Ns = 1 .<< (1:10)
@@ -67,8 +67,8 @@ end
 plt_m100 = plot(Ns, DFT_time, label = "DFT", xlabel = "N", ylabel = "time [sec]",
                 axis = :log10, minorgrid = true, markershape = :circle, title = "M = $M",
                 legend = :topleft)
-plot!(plt_m100, Ns, FFT_time, label = "FFT", markershape = :circle)
-plot!(plt_m100, Ns, cached_DFT_time, label = "DFT cached", markershape = :circle)
+plot!(plt_m100, Ns, FFT_time, label = "FFT", markershape = :rect)
+plot!(plt_m100, Ns, cached_DFT_time, label = "cached DFT", markershape = :star5)
 
 function apply_FFT(x::AbstractMatrix)
     x = convert.(ComplexF64, x)
@@ -119,7 +119,7 @@ for (i, N) in enumerate(Ns)
     local x = rand(ComplexF64, N, M)
     my_FFT_time[i] = @belapsed apply_FFT($x)
 end
-plot!(plt_m1, Ns, my_FFT_time, label = "FFT my", markershape = :circle)
+plot!(plt_m1, Ns, my_FFT_time, label = "my FFT", markershape = :diamond)
 
 M = 100
 Ns = 1 .<< (1:10)
@@ -129,7 +129,65 @@ for (i, N) in enumerate(Ns)
     local x = rand(ComplexF64, N, M)
     my_FFT_time[i] = @belapsed apply_DFT($x)
 end
-plot!(plt_m100, Ns, my_FFT_time, label = "FFT my", markershape = :circle)
+plot!(plt_m100, Ns, my_FFT_time, label = "my FFT", markershape = :diamond)
+
+function apply_FFT_with_cached_DFT(x::AbstractMatrix)
+    x = convert.(ComplexF64, x)
+    N = size(x, 1)
+    if N == 1
+        return x
+    end
+    y = similar(x)
+    ωₙ = cispi.(range(0; step = -2 / N, length = N >> 1))
+    apply_FFT_with_cached_DFT!(y, x, ωₙ, N)
+    return y
+end
+function apply_FFT_with_cached_DFT!(y::AbstractMatrix{ComplexF64},
+                                    x::AbstractMatrix{ComplexF64},
+                                    ωₙ::AbstractVector{ComplexF64},
+                                    N::Integer)
+    if N < 129 # 2⁷
+        i = trailing_zeros(N)
+        mul!(y, DFT_cache[i], x)
+        return
+    end
+    N₂ = N >> 1
+    yᵉᵛᵉⁿ = @view y[1:N₂, :]
+    yᵒᵈᵈ = @view y[(N₂ + 1):end, :]
+    xᵉᵛᵉⁿ = @view x[1:2:end, :]
+    xᵒᵈᵈ = @view x[2:2:end, :]
+    ωₙ₂ = @view ωₙ[1:2:end]
+    apply_FFT!(yᵉᵛᵉⁿ, xᵉᵛᵉⁿ, ωₙ₂, N₂)
+    apply_FFT!(yᵒᵈᵈ, xᵒᵈᵈ, ωₙ₂, N₂)
+    yᵒᵈᵈ .*= ωₙ
+    xₜₑₘₚ = @view x[1:N₂, :]
+    @. xₜₑₘₚ = yᵉᵛᵉⁿ - yᵒᵈᵈ
+    yᵉᵛᵉⁿ .+= yᵒᵈᵈ
+    yᵒᵈᵈ .= xₜₑₘₚ
+    nothing
+end
+
+Ns = 1 .<< (1:10)
+my_FFT_cached_DFT_time = similar(Ns, Float64)
+M = 1
+for (i, N) in enumerate(Ns)
+    println(i, " ", N)
+    local x = rand(ComplexF64, N, M)
+    my_FFT_cached_DFT_time[i] = @belapsed apply_FFT($x)
+end
+plot!(plt_m1, Ns, my_FFT_cached_DFT_time, label = "my FFT with cached DFT",
+      markershape = :hexagon)
+
+M = 100
+Ns = 1 .<< (1:10)
+my_FFT_time = similar(Ns, Float64)
+for (i, N) in enumerate(Ns)
+    println(i, " ", N)
+    local x = rand(ComplexF64, N, M)
+    my_FFT_time[i] = @belapsed apply_DFT($x)
+end
+plot!(plt_m100, Ns, my_FFT_cached_DFT_time, label = "my FFT with cached DFT",
+      markershape = :hexagon)
 
 savefig(plt_m1, "time_m1.png")
 savefig(plt_m100, "time_m100.png")
