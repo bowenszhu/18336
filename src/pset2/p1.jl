6fd705e768019451e26fd5123426f52e7d7fd7e6
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-10-10 16:35:13 -0400

    proj2 p1.4

--- src/pset2/p1.jl
+++ src/pset2/p1.jl
@@ -3,9 +3,10 @@ using LinearAlgebra: SymTridiagonal, Diagonal
 using SparseArrays: sparse, kron
 using Distributions: MvNormal, pdf
 using PDMats: ScalMat
-using Plots: contour, savefig, plot
+using Plots: contour, savefig, plot, plot!
 using LaTeXStrings: @L_str
-using FFTW: r2r, RODFT00
+using FFTW: r2r, RODFT00, r2r!
+using BenchmarkTools: @belapsed
 
 # 1 A fast free-space direct solver
 const c₀ = 299792458
@@ -36,8 +37,7 @@ helmholtz_op(8, 21.3e6)
 Σ = ScalMat(2, σ²)
 d = MvNormal(μ, Σ) # Gaussian impulse
 function v_op(xs::AbstractVector, ys::AbstractVector)
-    density = [pdf(d, [x, y]) for x in xs, y in ys]
-    # vec(density)
+    [pdf(d, [x, y]) for x in xs, y in ys]
 end
 N = 256
 h = 1 / N
@@ -46,15 +46,15 @@ ys = xs
 F = v_op(xs, ys)
 rhs = vec(F)
 fd2 = ∇²_op(N)
-f₁ = 21.3e6
-helmholtz₁ = fd2 + k²_op(N, f₁)
+f = 21.3e6
+helmholtz₁ = fd2 + k²_op(N, f)
 u₁ = helmholtz₁ \ rhs
 u₁ = reshape(u₁, (N - 1, N - 1))
 contour(xs, ys, u₁, fill = true, aspect_ratio = 1, lims = (0, 1), xlabel = L"x",
         ylabel = L"y", title = L"f=21.3\:\mathrm{MHz}")
 savefig("p121.svg")
-f₂ = 298.3e6
-helmholtz₂ = fd2 + k²_op(N, f₂)
+f = 298.3e6
+helmholtz₂ = fd2 + k²_op(N, f)
 u₂ = helmholtz₂ \ rhs
 u₂ = reshape(u₂, (N - 1, N - 1))
 contour(xs, ys, u₂, fill = true, aspect_ratio = 1, lims = (0, 1), xlabel = L"x",
@@ -65,10 +65,52 @@ savefig("p122.svg")
 F̂ = r2r(F, RODFT00)
 λ = cospi.(range(h, 1 - h, N - 1))
 @. λ = 2.0 - 2.0 * λ
-k² = (2π * f₂ / c₀)^2
+k² = (2π * f / c₀)^2
 h² = h^2
-Û = [F̂[i, j] / (k² - (λ[i] + λ[j]) / h²) for i in 1:(N - 1), j in 1:(N - 1)]
-U = r2r(Û, RODFT00) / (2 * N)^2
+for i in 1:(N - 1), j in 1:(N - 1)
+    F̂[i, j] /= k² - (λ[i] + λ[j]) / h²
+end
+U = F̂
+r2r!(U, RODFT00)
+U ./= (2 * N)^2
 contour(xs, ys, U, fill = true, aspect_ratio = 1, lims = (0, 1), xlabel = L"x",
         ylabel = L"y", title = L"f=298.3\:\mathrm{MHz}")
 savefig("p13.svg")
+
+## Problem 1.4
+function setup_rhs(N::Integer)
+    h = 1 / N
+    xs = range(h, 1 - h, N - 1)
+    ys = xs
+    v_op(xs, ys)
+end
+function sparse_solve(N::Integer, F::AbstractMatrix)
+    fd2 = ∇²_op(N)
+    helmholtz_op = fd2 + Diagonal(Fill(k², (N - 1)^2))
+    U = helmholtz_op \ vec(F)
+    reshape(U, (N - 1, N - 1))
+end
+function dst_precondition(N::Integer, F::AbstractMatrix)
+    UF = r2r(F, RODFT00) # F̂
+    λ = cospi.(range(h, 1 - h, N - 1))
+    @. λ = 2.0 - 2.0 * λ
+    h² = 1 / N^2
+    for i in 1:(N - 1), j in 1:(N - 1)
+        UF[i, j] /= k² - (λ[i] + λ[j]) / h² # Û
+    end
+    r2r!(UF, RODFT00) # U
+    UF ./= (2N)^2 # normalize
+    UF
+end
+Ns = 1 .<< (4:15)
+sp_time = similar(Ns, Float64)
+dst_time = similar(sp_time)
+for (i, N) in enumerate(Ns)
+    local F = setup_rhs(N)
+    sp_time[i] = @belapsed sparse_solve(N, F)
+    dst_time[i] = @belapsed dst_precondition(N, F)
+end
+plot(Ns, sp_time, axis = :log, xlabel = L"N", ylabel = "time [sec]",
+     label = "sparse solve", markershape = :circle, legend = :left)
+plot!(Ns, dst_time, label = "DST precondition", markershape = :circle)
+savefig("p14.svg")
