27f0898de783b15e3c79aa9070ded8d891aee27b
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-11-01 21:40:41 -0400

    proj3 p2.3 update

--- src/pset3/p2.jl
+++ src/pset3/p2.jl
@@ -51,9 +51,9 @@ plot(norm_v, yaxis = :log10, legend = false, xlabel = L"n",
 savefig("p22.svg")
 
 # Problem 2.3
-∂ₓu₁ = (u[1] - u[2]) / (x[1] - x[2])
+∂ₓu₁ = u[2] / (1.0 - x[2])
 const KG = 6.34e29
-M = 4 / √π * KG * ∂ₓu₁
+M = -4 / √π * KG * ∂ₓu₁
 Mₒ = 1.99e30
 mass = M / Mₒ
 open("p23.txt", "w") do io
