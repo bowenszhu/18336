001189b9c7674acd5b713b626239b196ee22e246
Bowen S. Zhu <bszhu@fas.harvard.edu>
2022-12-01 18:10:31 -0500

    pset5 p1b

--- src/pset5/p1.jl
+++ src/pset5/p1.jl
@@ -34,3 +34,33 @@ for (i, ξ) in enumerate(ξs)
 end
 plot(ξs, ranks, axis = :log, legend = false, xlabel = L"ξ", ylabel = "relative rank")
 savefig("p1a.svg")
+
+ξ = 6.0
+Ns = 20:10:200
+ranks = zeros(Int, length(Ns))
+for (i, N) in enumerate(Ns)
+    local M = N
+    local Y = rand(3, M)
+    local X = rand(3, N)
+    X[1, :] .+= ξ
+    local G = Matrix{Float64}(undef, M, N)
+    for m in 1:M
+        y = @view Y[:, m]
+        for n in 1:N
+            x = @view X[:, n]
+            G[m, n] = norm(y - x)
+        end
+    end
+    @. G = 1.0 / G
+    vals = svdvals(G)
+    ϵsnorm = ϵ * vals[1]
+    for val in vals
+        if val < ϵsnorm
+            break
+        end
+        ranks[i] += 1
+    end
+end
+plot(Ns, ranks, legend = false, xlabel = "number of sources/targets",
+     ylabel = "relative rank")
+savefig("p1b.svg")
